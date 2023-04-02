e68c4956ecb5a80e8c9c23bf49c85bac8c0caabe
Ademir Guilherme <atguilherme@gmail.com>
2023-04-02 19:41:51 -0300

    fix li bug

--- src/components/CartList/CartItem/index.jsx
+++ src/components/CartList/CartItem/index.jsx
@@ -3,26 +3,26 @@ import { StyledItem } from "./cartItem";
 
 export const CartItem = ({ cartItem, removeProductFromCart }) => {
   return (
-    <StyledItem>
-      <li key={cartItem.id}>
-        <div className="cart__item">
-          <div className="cart__imgContainer">
-            <img src={cartItem.img} alt={cartItem.name} />
-          </div>
-          <div className="cart__produtData">
-            <h2 className="heading3">{cartItem.name}</h2>
-            <h3 className="caption">{cartItem.category}</h3>
-          </div>
-          <div className="cart__buttonContainer">
-            <StyledButton
-              className="caption600 cart__buttonContainer"
-              btnStyle={"btn-sm-gray"}
-              type="button"
-              onClick={() => removeProductFromCart(cartItem.id)}
-            >Remover</StyledButton>
-          </div>
+    <StyledItem key={cartItem.id}>
+      <div className="cart__item">
+        <div className="cart__imgContainer">
+          <img src={cartItem.img} alt={cartItem.name} />
         </div>
-      </li>
+        <div className="cart__produtData">
+          <h2 className="heading3">{cartItem.name}</h2>
+          <h3 className="caption">{cartItem.category}</h3>
+        </div>
+        <div className="cart__buttonContainer">
+          <StyledButton
+            className="caption600 cart__buttonContainer"
+            btnStyle={"btn-sm-gray"}
+            type="button"
+            onClick={() => removeProductFromCart(cartItem.id)}
+          >
+            Remover
+          </StyledButton>
+        </div>
+      </div>
     </StyledItem>
   );
 };
